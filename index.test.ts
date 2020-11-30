51195b9b0815edc14d6111b0ad5625b6a1c163c0
Cameron Martin <cameron.martin@forgerock.com>
2020-11-30 18:26:00 +0000

    Initial commit

---
+++ index.test.ts
@@ -0,0 +1,22 @@
+import { SqlClient } from './index';
+import { expectType, TypeOf, TypeEqual } from "ts-expect";
+
+interface TestSchema {
+    users: {
+        foo: number,
+        bar: string,
+        baz: boolean,
+    }
+}
+
+test('simple select statement', () => {
+    let result = new SqlClient<TestSchema>().query('SELECT foo, bar FROM users');
+
+    expectType<TypeEqual<{ foo: number, bar: string }[], typeof result>>(true);
+});
+
+test('select statement with where clause', () => {
+    let result = new SqlClient<TestSchema>().query('SELECT foo, bar, baz FROM users WHERE bar = "hello"');
+
+    expectType<TypeEqual<{ foo: number, bar: string, baz: boolean }[], typeof result>>(true);
+});
