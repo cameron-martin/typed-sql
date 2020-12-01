e1d41242ed1a2dbf921abce32019e7a8c6076aa6
Cameron Martin <cameron.martin@forgerock.com>
2020-12-01 16:21:09 +0000

    Add string literal parsing

--- src/parser-library.test.ts
+++ src/parser-library.test.ts
@@ -1,10 +1,10 @@
-import { ParseSuccess, ParseFail, ParseSingleWhitespace, ParseManyWhitespace, ParseMany1Whitespace } from './parser-library';
+import { ParseSuccess, ParseFail, ParseSingleWhitespace, ParseManyWhitespace, ParseMany1Whitespace, ParseStringLiteral } from './parser-library';
 import { expectType, TypeOf, TypeEqual } from "ts-expect";
 
 describe('ParseSingleWhitespace', () => {
     it('parses a single space from the front', () => {
         type Result = ParseSingleWhitespace<"   ">;
-        type Expected = ParseSuccess<string, " ", "  ">;
+        type Expected = ParseSuccess<" ", "  ">;
     
         expectType<TypeEqual<Result, Expected>>(true);
     });
@@ -19,15 +19,98 @@ describe('ParseSingleWhitespace', () => {
 describe('ParseManyWhitespace', () => {
     it('parses empty string and consumes nothing if no whitespace at front', () => {
         type Result = ParseManyWhitespace<"foo ">;
-        type Expected = ParseSuccess<string, "", "foo ">;
+        type Expected = ParseSuccess<"", "foo ">;
     
         expectType<TypeEqual<Result, Expected>>(true);
     });
 
     // it('parses and consumes all whitespace characters from start of string', () => {
-    //     type Result = ParseMany1Whitespace<"  foo bar">;
-    //     type Expected = ParseSuccess<string, "  ", "foo bar">;
+    //     type Result = ParseManyWhitespace<" foo bar">;
+    //     type Expected = ParseSuccess<" ", "foo bar">;
     
     //     expectType<TypeEqual<Result, Expected>>(true);
     // });
+});
+
+describe('ParseStringLiteral', () => {
+    describe('single quotes', () => {
+        it('parses empty string with no remainder', () => {
+            type Result = ParseStringLiteral<"''">;
+            type Expected = ParseSuccess<"", "">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+
+        it('parses empty string with remainder', () => {
+            type Result = ParseStringLiteral<"'' foo">;
+            type Expected = ParseSuccess<"", " foo">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+
+        it('parses simple string', () => {
+            type Result = ParseStringLiteral<"'foo' bar">;
+            type Expected = ParseSuccess<"foo", " bar">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+
+        it('parses string with escaped quote', () => {
+            type Result = ParseStringLiteral<"'foo\\'' bar">;
+            type Expected = ParseSuccess<"foo'", " bar">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+
+        it('parses string with escaped non-quote value', () => {
+            type Result = ParseStringLiteral<"'fo\\o' bar">;
+            type Expected = ParseSuccess<"foo", " bar">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+    });
+
+    describe('double quotes', () => {
+        it('parses empty string with no remainder', () => {
+            type Result = ParseStringLiteral<'""'>;
+            type Expected = ParseSuccess<"", "">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+
+        it('parses empty string with remainder', () => {
+            type Result = ParseStringLiteral<'"" foo'>;
+            type Expected = ParseSuccess<"", " foo">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+
+        it('parses simple string', () => {
+            type Result = ParseStringLiteral<'"foo" bar'>;
+            type Expected = ParseSuccess<"foo", " bar">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+
+        it('parses string with escaped quote', () => {
+            type Result = ParseStringLiteral<'"foo\\"" bar'>;
+            type Expected = ParseSuccess<"foo\"", " bar">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+
+        it('parses string with escaped non-quote value', () => {
+            type Result = ParseStringLiteral<'"fo\\o" bar'>;
+            type Expected = ParseSuccess<"foo", " bar">;
+        
+            expectType<TypeEqual<Result, Expected>>(true);
+        });
+    });
+
+    it('does not parse mixed quotes', () => {
+        type Result = ParseStringLiteral<'"foo\' bar'>;
+        type Expected = ParseFail;
+    
+        expectType<TypeEqual<Result, Expected>>(true);
+    });
 });
--- src/parser-library.ts
+++ src/parser-library.ts
@@ -1,27 +1,63 @@
-export type ParseSuccess<T, X extends T, R extends string> = { success: true, result: X, rest: R };
-export type ParseFail = { success: false };
-
-export type ParseLiteral<Literal extends string, S extends string> = S extends `${Literal}${infer R}` ? ParseSuccess<string, Literal, R> : ParseFail;
+/**
+ * A specialisation of ParseSuccess where X has an added constraint of being a string.
+ * Useful when using in a condition type as `Y extends ParseSuccessString<infer X, infer R> ? ... : ...`
+ * since then X is usable in template literal types.
+ */
+export type ParseSuccessString<X extends string, R extends string> = ParseSuccess<X, R>;
 
 /**
- * Consumes a single character of whitespace.
+ * Represents a successful parse, yielding the value `X` with remaining input `R`.
+ */
+export type ParseSuccess<X, R extends string> = { success: true, result: X, rest: R };
+
+/**
+ * Represents a failed parse.
+ */
+export type ParseFail = { success: false };
+
+/**
+ * Consumes a literal value, yielding that value.
+ */
+export type ParseLiteral<Literal extends string, S extends string> = S extends `${Literal}${infer R}` ? ParseSuccess<Literal, R> : ParseFail;
+
+/**
+ * Consumes a single character of whitespace, yielding that character.
  * TODO: Add more whitespace chars
  */
 export type ParseSingleWhitespace<S extends string> =
-  S extends ` ${infer R}` ? ParseSuccess<string, ' ', R> :
-  S extends `\t${infer R}` ? ParseSuccess<string, '\t', R> :
+  S extends ` ${infer R}` ? ParseSuccess<' ', R> :
+  S extends `\t${infer R}` ? ParseSuccess<'\t', R> :
   ParseFail;
 
 /**
- * Consumes 0 or more characters of whitespace as possible, returning the consumed whitespace.
+ * Consumes 0 or more characters of whitespace as possible, yielding the consumed whitespace.
  */
 export type ParseManyWhitespace<S extends string> =
-  ParseSingleWhitespace<S> extends ParseSuccess<string, infer T1, infer R0> ?
-  (ParseManyWhitespace<R0> extends ParseSuccess<string, infer T2, infer R1> ? ParseSuccess<string, `${T1}${T2}`, R1> : ParseSuccess<string, T1, R0> ) : ParseSuccess<string, '', S>;
+  ParseSingleWhitespace<S> extends ParseSuccessString<infer X1, infer R0> ?
+  (ParseSingleWhitespace<R0> extends ParseSuccessString<infer X2, infer R1> ? ParseSuccess<`${X1}${X2}`, R1> : ParseSuccess<X1, R0> ) : ParseSuccess<'', S>;
 
 /**
- * Consumes 1 or more characters of whitespace as possible, returning the consumed whitespace.
+ * Consumes 1 or more characters of whitespace as possible, yielding the consumed whitespace.
  */
 export type ParseMany1Whitespace<S extends string> =
-  ParseSingleWhitespace<S> extends ParseSuccess<string, infer T1, infer R0> ?
-  (ParseManyWhitespace<R0> extends ParseSuccess<string, infer T2, infer R1> ? ParseSuccess<string, `${T1}${T2}`, R1> : ParseSuccess<string, T1, R0> ) : ParseFail;
+  ParseSingleWhitespace<S> extends ParseSuccessString<infer X1, infer R0> ?
+  (ParseMany1Whitespace<R0> extends ParseSuccessString<infer X2, infer R1> ? ParseSuccess<`${X1}${X2}`, R1> : ParseSuccess<X1, R0>) : ParseFail;
+
+/**
+ * Parse a string literal in single or double quotes, with backslash-escaping
+ */
+export type ParseStringLiteral<S extends string> =
+  ParseStringLiteralWithQuote<"'", S> extends ParseSuccess<infer X, infer R> ? ParseSuccess<X, R> :
+  ParseStringLiteralWithQuote<'"', S> extends ParseSuccess<infer X, infer R> ? ParseSuccess<X, R> :
+  ParseFail;
+
+type ParseStringLiteralWithQuote<Quote extends string, S extends string> =
+  S extends `${Quote}${infer R0}`
+  ? ParseInsideStringLiteralWithQuote<Quote, R0>
+  : ParseFail;
+
+type ParseInsideStringLiteralWithQuote<Quote extends string, S extends string> =
+  S extends `${Quote}${infer R0}` ? ParseSuccess<'', R0> :
+  S extends `\\${infer X0}${infer R0}` ? (ParseInsideStringLiteralWithQuote<Quote, R0> extends ParseSuccessString<infer X1, infer R1> ? ParseSuccess<`${X0}${X1}`, R1> : ParseFail) :
+  S extends `${infer X0}${infer R0}` ? (ParseInsideStringLiteralWithQuote<Quote, R0> extends ParseSuccessString<infer X1, infer R1> ? ParseSuccess<`${X0}${X1}`, R1> : ParseFail) :
+  ParseFail
